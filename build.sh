1907cc1fc07f2060e7366dd8abce17d4b600e228
Sam Wolfson <terabyte128@gmail.com>
2025-08-07 21:58:59 -0700

    minify and include source maps for debugging

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 
+set -euo pipefail
+
 DIRS=(
     public-footer
     admin-footer
 )
 
 for dir in "${DIRS[@]}"; do
-    cat "$dir/"*.ts | yarn esbuild --target=es2015 --bundle --outfile="dist/$dir.js"
+    cat "$dir/"*.ts | yarn esbuild --sourcemap --minify --target=es2015 --bundle --outfile="dist/$dir.js"
 done
